92fd58e2c8587f1284bcfada1106bf19bdabcc13
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-07 15:31:08 +0700

    create fungsi main class mahasiwa

--- PointerdanFriendclass_0231/project2.cpp
+++ PointerdanFriendclass_0231/project2.cpp
@@ -4,10 +4,22 @@ using namespace std;
 class mahasiswa {
 public:
 	int nim;
-	
+	int showNim() {
+		cout << "no induk = " << nim << endl;
+	}
 };
 
 int main() {
 
 	mahasiswa mhs{ 1 };
+	mhs.showNim();
+
+	mahasiswa &refmhs = mhs;
+	refmhs.nim = 2;
+	mhs.showNim();
+
+	mahasiwa*pMhs = &mhs;
+	pMhs->nim = 3;
+
+
 }
